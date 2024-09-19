eba0ff221748e926f9efae9bad8f5a0743bfbe02
Meshal Khan <meshalgohar652@gmail.com>
2024-09-19 16:00:10 +0500

    'hotfix'

--- plugins/MauticRandomSmtpBundle/Config/config.php
+++ plugins/MauticRandomSmtpBundle/Config/config.php
@@ -7,7 +7,7 @@ return [
     'version'     => '1.0.0',
     'routes' => [
         'main' => [
-            'mautic_random_smtp_preview' => [
+            'mautic_random_smtp_action' => [
                 'path'       => '/random-smtp/preview',
                 'controller' => \MauticPlugin\MauticRandomSmtpBundle\Controller\RandomSmtpController::class . '::previewAction',
             ],
@@ -15,42 +15,36 @@ return [
     ],
     'services' => [
         'forms' => [
-            'MauticPlugin\MauticRandomSmtpBundle\Form\Type\SmtpPreviewType' => [
-                'arguments' => [
-                    '@Mautic\PluginBundle\Helper\IntegrationHelper',
-                ],
+            'mautic.random_smtp.form.type.preview' => [
+                'class' => \MauticPlugin\MauticRandomSmtpBundle\Form\Type\SmtpPreviewType::class,
             ],
         ],
         'helpers' => [],
-        'other'   => [
+        'other' => [
             'mautic.transport.random' => [
                 'class'        => \MauticPlugin\MauticRandomSmtpBundle\Swiftmailer\Transport\RandomSmtpTransport::class,
                 'arguments'    => [
-                    '@mautic.random.smtp.randomizer',
-                    '@monolog.logger.mautic',
+                    'mautic.random.smtp.randomizer',
+                    'monolog.logger.mautic',
                 ],
-                'tags'         => [
-                    [
-                        'name' => 'mautic.email_transport',
-                        'alias' => 'mautic.email.config.mailer_transport.random_smtp',
-                    ],
+                'tags' => [
+                    ['name' => 'mautic.email_transport', 'alias' => 'mautic.email.config.mailer_transport.random_smtp'],
                 ],
             ],
             'mautic.random.smtp.randomizer' => [
                 'class'        => \MauticPlugin\MauticRandomSmtpBundle\Randomizer\SmtpRandomizer::class,
                 'arguments'    => [
-                    '@mautic.helper.integration',
+                    'mautic.helper.integration',
                 ],
             ],
         ],
-        'models'       => [],
+        'models' => [],
         'integrations' => [
             'mautic.integration.random_smtp' => [
                 'class' => \MauticPlugin\MauticRandomSmtpBundle\Integration\RandomSmtpIntegration::class,
             ],
         ],
     ],
-    'menu'       => [],
+    'menu' => [],
     'parameters' => [],
 ];
-
